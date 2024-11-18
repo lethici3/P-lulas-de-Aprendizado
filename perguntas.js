6dd8aee164901248198f6e457cdb85e9a6ee4337
lethici3 <00001087677695sp@al.educacao.sp.gov.br>
2024-11-18 18:25:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fisica',
+    'Qual é a diferença entre calor e temperatura?',
+    'Calor é a energia transferida de um corpo para outro devido à diferença de temperatura. Temperatura é a medida do grau de agitação das partículas de um corpo.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano aconteceu a independência do Brasil?',
+    'A independência do Brasil foi declarada em 7 de setembro de 1822.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a diferença entre ligação covalente e ligação iônica?',
+    'Ligação covalente: Compartilhamento de elétrons entre átomos. Ligação iônica: Transferência de elétrons de um átomo para outro, formando íons que se atraem.'
+)
+
+criaCartao(
+    'Português',
+    'Qual a diferença entre uma vírgula e um ponto e vírgula?',
+    'Vírgula Usada para separar elementos dentro de uma oração, como enumerar itens, separar orações coordenadas e isolar vocativos. Ponto e vírgula Usado para separar orações independentes que são mais relacionadas entre si, ou para separar itens de uma lista quando esses itens já contêm vírgulas.'
+)
